bb7a1e6f8c8a90aea7d21f04dabac269ad057d8a
ngys9919 <ngys9919@yahoo.com>
2025-02-16 14:56:29 +0800

    Add cool face API

--- index-original.js
+++ index-original.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const cool = require('cool-ascii-faces')
 
 const port = process.env.PORT || 5006
 
@@ -13,11 +12,6 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   console.log(`Rendering 'pages/index' for route '/'`)
   res.render('pages/index')
-})
-
-app.get('/cool', (req, res) => {
-  console.log(`Rendering a cool ascii face for route '/cool'`)
-  res.send(cool())
 })
 
 const server = app.listen(port, () => {
